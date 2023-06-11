921a00613f72bb458a0f82b9c7522ad0d488afa5
pramod54 <princechamoli22@gmail.com>
2023-06-11 08:22:06 +0000

    added wsgi connection for open shift backend deployment

--- indigo_hack/indigo_hack/wsgi.py
+++ indigo_hack/indigo_hack/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indigo_hack.settings')
 
 application = get_wsgi_application()
